f5f22fb0884c2b570487aff72c8077cc9f5ce817
elberthuang <huangyou@idriverplus.com>
2017-03-15 20:53:39 +0800

    Update backpack_3d.lua

--- cartographer_ros/configuration_files/backpack_3d.lua
+++ cartographer_ros/configuration_files/backpack_3d.lua
@@ -24,23 +24,23 @@ options = {
   use_odometry = false,
   use_laser_scan = false,
   use_multi_echo_laser_scan = false,
-  num_point_clouds = 2,
+  num_point_clouds = 1,
   lookup_transform_timeout_sec = 0.2,
   submap_publish_period_sec = 0.3,
   pose_publish_period_sec = 5e-3,
 }
 
-TRAJECTORY_BUILDER_3D.scans_per_accumulation = 160
+TRAJECTORY_BUILDER_3D.scans_per_accumulation = 1
 
 MAP_BUILDER.use_trajectory_builder_3d = true
 MAP_BUILDER.num_background_threads = 7
 MAP_BUILDER.sparse_pose_graph.optimization_problem.huber_scale = 5e2
-MAP_BUILDER.sparse_pose_graph.optimize_every_n_scans = 320
+MAP_BUILDER.sparse_pose_graph.optimize_every_n_scans = 640
 MAP_BUILDER.sparse_pose_graph.constraint_builder.sampling_ratio = 0.03
 MAP_BUILDER.sparse_pose_graph.optimization_problem.ceres_solver_options.max_num_iterations = 10
 -- Reuse the coarser 3D voxel filter to speed up the computation of loop closure
 -- constraints.
 MAP_BUILDER.sparse_pose_graph.constraint_builder.adaptive_voxel_filter = TRAJECTORY_BUILDER_3D.high_resolution_adaptive_voxel_filter
-MAP_BUILDER.sparse_pose_graph.constraint_builder.min_score = 0.62
+MAP_BUILDER.sparse_pose_graph.constraint_builder.min_score = 0.5
 
 return options
